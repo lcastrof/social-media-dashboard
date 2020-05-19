33f1d6e483aad186fda7761677603ba830d86fdb
Lucas de Castro <castro.lucas@engenharia.ufjf.br>
2020-05-18 23:49:14 -0300

    Icon fixes

--- src/components/MiniCard/index.js
+++ src/components/MiniCard/index.js
@@ -9,12 +9,12 @@ import arrowDown from '../../images/icon-down.svg';
 
 import { Container, Bottom } from './styles';
 
-const MiniCard = ({ type, name, number, percentage, arrow }) => {
+const MiniCard = ({ social, name, number, percentage, arrow }) => {
 
-  function getIcon(type) {
+  function getIcon(social) {
     let icon = facebook;
 
-    switch (type) {
+    switch (social) {
       case 'twitter':
         icon = twitter;
         break;
@@ -41,14 +41,14 @@ const MiniCard = ({ type, name, number, percentage, arrow }) => {
   }
 
   return (
-    <Container type={type}>
+    <Container social={social}>
       <div className="items">
-        <p className="type">{`${name}`}</p>
+        <p className="social">{`${name}`}</p>
         <p className="number">{number}</p>
       </div>
       <div className="iconPercentage">
-        <img src={getIcon(type)} alt={type}/>
-        <Bottom arrowType={arrow}>
+        <img src={getIcon(social)} alt={social}/>
+        <Bottom arrowsocial={arrow}>
           <img src={getArrow(arrow)} alt="arrow icon"/>
           <p className="percentageNumber">{percentage}</p>
         </Bottom>
